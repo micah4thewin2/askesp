5d5e9ec759eb492f518d6313271f515bea44dfee
micah4thewin <contact@micahlevason.com>
2023-08-30 11:03:55 -0700

    More directory changes, content and styles.

--- src/js/governance.js
+++ src/js/governance.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Typed.js initialization
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 40,
+    backSpeed: 20,
+    loop: true
+  });
 
-  // Change the copyright
-  const copyRight = document.getElementById("copyright"); // Auto Update Copyright
+  // Update for Governance page
+  if (window.location.pathname === '/governance') {
+    // Additional JS code specific to the Governance page can go here
+  }
+
+  // Existing code
+  const copyRight = document.getElementById("copyright");
   const currentYear = new Date().getFullYear();
   copyRight.innerHTML = "ASK ESP ©" + currentYear;
   AOS.init();
-
-
 });
